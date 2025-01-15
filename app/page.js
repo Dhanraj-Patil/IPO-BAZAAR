c700965bb02089380d81756c2c0565c7d5ef16ba
yash <yashheda777@gmail.com>
2025-01-15 10:47:01 +0530

    fixed glitch

--- app/page.js
+++ app/page.js
@@ -3,15 +3,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-[100vh] w-[80%] mx-auto  justify-between items-center ">
-      <div className="flex justify-between items-center translate-y-[-2rem] ">
-        <div className="pl-[1rem]  w-[45%]">
-          <TableDemo/>
+    <div className="h-[100vh] w-[80%] mx-auto justify-between items-center">
+      <div className="flex justify-between items-center">
+        <div className="pl-[1rem] w-[45%]">
+          <TableDemo />
         </div>
-        
-        <Image src="/Assets/BB.png" width={700} height={10} alt="Background" className="translate-x-[2rem]  pt-2"  />
-       
-      
+
+        <div className="translate-x-[2rem] pt-2">
+          <Image
+            src="/Assets/BB.png"
+            width={700}
+            height={10}
+            alt="Background"
+            priority
+          />
+        </div>
       </div>
     </div>
   );
